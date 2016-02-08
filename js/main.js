8d5e16c5661510d286700467e0670348f4dda994
Hai Tan <haitannyc@gmail.com>
2016-02-08 16:53:50 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Variable types and Examples
+var number = 3;
+number *= 20;
+console.log(number);
+
+// alert to open panel
+alert("hello world");
+
+// print to console
+console.log("The comment prints");
+
